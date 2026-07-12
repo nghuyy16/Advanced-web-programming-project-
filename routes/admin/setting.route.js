969af1dc03f1b0f4276b6d588085025ba4435c0c
Manx-bit <manx7082@gmail.com>
2026-07-12 10:57:06 +0000

    1

---
+++ routes/admin/setting.route.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const settingController = require("../../controllers/admin/setting.controller");
+const multer  = require('multer');
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
+const upload = multer({ storage: cloudinaryHelper.storage });
+
+router.get('/list', settingController.list)
+
+router.get('/website-info', settingController.websiteInfo)
+
+router.patch(
+  '/website-info', 
+  upload.fields([
+    { name: 'logo', maxCount: 1 }, 
+    { name: 'favicon', maxCount: 1 }
+  ]),
+  settingController.websiteInfoPatch
+)
+
+router.get('/account-admin/list', settingController.accountAdminList)
+
+router.get('/account-admin/create', settingController.accountAdminCreate)
+
+router.post(
+  '/account-admin/create', 
+  upload.single('avatar'),
+  settingController.accountAdminCreatePost
+)
+
+router.get('/account-admin/edit/:id', settingController.accountAdminEdit)
+
+router.patch(
+  '/account-admin/edit/:id', 
+  upload.single('avatar'),
+  settingController.accountAdminEditPatch
+)
+
+router.get('/role/list', settingController.roleList)
+
+router.get('/role/create', settingController.roleCreate)
+
+router.post('/role/create', settingController.roleCreatePost)
+
+router.get('/role/edit/:id', settingController.roleEdit)
+
+router.patch('/role/edit/:id', settingController.roleEditPatch)
+
+module.exports = router;
