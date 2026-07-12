ec16458244e2eed9d7ac3b70d2cbcee4433faaed
Manx-bit <manx7082@gmail.com>
2026-07-12 10:55:17 +0000

    1

---
+++ routes/admin/index.route.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const accountRoutes = require("./account.route");
+const dashboardRoutes = require("./dashboard.route");
+const categoryRoutes = require("./category.route");
+const tourRoutes = require("./tour.route");
+const orderRoutes = require("./order.route");
+const userRoutes = require("./user.route");
+const contactRoutes = require("./contact.route");
+const settingRoutes = require("./setting.route");
+const profileRoutes = require("./profile.route");
+const uploadRoutes = require("./upload.route");
+
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
+
+router.use('/account', accountRoutes)
+router.use('/dashboard', authMiddleware.verifyToken, dashboardRoutes)
+router.use('/category', authMiddleware.verifyToken, categoryRoutes)
+router.use('/tour', authMiddleware.verifyToken, tourRoutes)
+router.use('/order', authMiddleware.verifyToken, orderRoutes)
+router.use('/user', authMiddleware.verifyToken, userRoutes)
+router.use('/contact', authMiddleware.verifyToken, contactRoutes)
+router.use('/setting', authMiddleware.verifyToken, settingRoutes)
+router.use('/profile', authMiddleware.verifyToken, profileRoutes)
+router.use('/upload', authMiddleware.verifyToken, uploadRoutes)
+
+router.use(authMiddleware.verifyToken, (req, res) => {
+  res.render("admin/pages/error-404", {
+    pageTitle: "404 Not Found"
+  });
+})
+
+module.exports = router;
