ff0ca57a00acda731439dc65d66af399260bef6a
Manx-bit <manx7082@gmail.com>
2026-07-12 10:55:57 +0000

    1

---
+++ routes/admin/order.route.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const orderController = require("../../controllers/admin/order.controller");
+
+router.get('/list', orderController.list)
+
+router.get('/edit/:id', orderController.edit)
+
+router.patch('/edit/:id', orderController.editPatch)
+
+module.exports = router;
