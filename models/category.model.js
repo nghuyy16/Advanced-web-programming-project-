33e9f7934f1ff8ae4578c252e76e8cffa6b2f9e5
NGHUYYY16 <nguyenhuy1603006@gmail.com>
2026-07-28 21:46:13 +0700

    fix category

--- models/category.model.js
+++ models/category.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-updater');
 mongoose.plugin(slug);
-
+// tạo schema cho danh mục 
 const schema = new mongoose.Schema({
   name: String,
   parent: String,
